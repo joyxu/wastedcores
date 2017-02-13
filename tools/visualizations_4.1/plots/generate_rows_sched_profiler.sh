5236605281a217449e9b51eecf726a231686bdf2
sonicyang <sonic.tw.tp@gmail.com>
2017-02-13 07:56:08 -0800

    Plotting the res:1us length:0.1ms

--- tools/visualizations_4.1/plots/generate_rows_sched_profiler.sh
+++ tools/visualizations_4.1/plots/generate_rows_sched_profiler.sh
@@ -15,24 +15,24 @@ generate_sched_profiler_graphs_all_parallel()
         ${COMPILER}                                                            \
         ./parse_rows_sched_profiler.php                                        \
         output/${5}_standard.png                                               \
-        $3 60500 -1 0 standard $1 $2 $4 &
+        $3 100000 -1 0 standard $1 $2 $4 &
 
     cat $INPUT |                                                               \
         ${COMPILER}                                                            \
         ./parse_rows_sched_profiler.php                                        \
         output/${5}_load.png                                                   \
-        $3 60500 -1 0 load $1 $2 $4 &
+        $3 100000 -1 0 load $1 $2 $4 &
 }
 
 # Generic runqueue and load graphs
-generate_sched_profiler_graphs_all_parallel 0 -1 600 nothing generic
+generate_sched_profiler_graphs_all_parallel 0 -1 1 nothing generic
 
 # Considered wakeups for core zero
-generate_sched_profiler_graphs_all_parallel 201 0 600 nothing wakeups
+generate_sched_profiler_graphs_all_parallel 201 0 1 nothing wakeups
 
 # Graph with threads movements and "overloaded wakeups"
-generate_sched_profiler_graphs_all_parallel 0 -1 600 arrows arrows
-generate_sched_profiler_graphs_all_parallel 0 -1 600 bad_wakeups               \
+generate_sched_profiler_graphs_all_parallel 0 -1 1 arrows arrows
+generate_sched_profiler_graphs_all_parallel 0 -1 1 bad_wakeups               \
                                                      overloaded_wakeups
 
 wait
